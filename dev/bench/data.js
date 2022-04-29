6de67fb84f75ec595a2c17471ad1ce13eb663b28
github-action-benchmark <github@users.noreply.github.com>
2022-04-29 18:23:10 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 4e14b88154da77d88d68429c12feb0bb17c92496

---
+++ dev/bench/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1651256590385,
+  "repoUrl": "https://github.com/connorjward/firedrake",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "c.ward20@imperial.ac.uk",
+            "name": "Connor Ward",
+            "username": "connorjward"
+          },
+          "committer": {
+            "email": "c.ward20@imperial.ac.uk",
+            "name": "Connor Ward",
+            "username": "connorjward"
+          },
+          "distinct": true,
+          "id": "4e14b88154da77d88d68429c12feb0bb17c92496",
+          "message": "fix",
+          "timestamp": "2022-04-29T19:22:38+01:00",
+          "tree_id": "bec9cfe35be2c94a36ef34570e945985e0b8d36f",
+          "url": "https://github.com/connorjward/firedrake/commit/4e14b88154da77d88d68429c12feb0bb17c92496"
+        },
+        "date": 1651256589967,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_demo.py::test_my_stuff1",
+            "value": 16575.777040989353,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000002642002495214567",
+            "extra": "mean: 60.32899679617754 usec\nrounds: 11549"
+          },
+          {
+            "name": "benchmarks/test_demo.py::test_my_stuff2",
+            "value": 467.8594163951414,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000024595786234612508",
+            "extra": "mean: 2.137394193548574 msec\nrounds: 465"
+          },
+          {
+            "name": "benchmarks/test_demo.py::test_my_stuff3",
+            "value": 1159.9730082363458,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000043971856640940085",
+            "extra": "mean: 862.0890252613955 usec\nrounds: 1148"
+          }
+        ]
+      }
+    ]
+  }
+}
