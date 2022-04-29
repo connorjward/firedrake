9cc0a157f0e0fda040236df2c44c462b39fb8ace
github-action-benchmark <github@users.noreply.github.com>
2022-04-29 18:30:58 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 88cec3b5f7b8b5117ef96b113755eb209bc5b77c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1651256590385,
+  "lastUpdate": 1651257058209,
   "repoUrl": "https://github.com/connorjward/firedrake",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000043971856640940085",
             "extra": "mean: 862.0890252613955 usec\nrounds: 1148"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "c.ward20@imperial.ac.uk",
+            "name": "Connor Ward",
+            "username": "connorjward"
+          },
+          "committer": {
+            "email": "c.ward20@imperial.ac.uk",
+            "name": "Connor Ward",
+            "username": "connorjward"
+          },
+          "distinct": true,
+          "id": "88cec3b5f7b8b5117ef96b113755eb209bc5b77c",
+          "message": "Modify test benchmark values",
+          "timestamp": "2022-04-29T19:30:25+01:00",
+          "tree_id": "2f7bf0257a13547bcbcd7c608249e80d106a6ae5",
+          "url": "https://github.com/connorjward/firedrake/commit/88cec3b5f7b8b5117ef96b113755eb209bc5b77c"
+        },
+        "date": 1651257057721,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_demo.py::test_my_stuff1",
+            "value": 920.7821992370436,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000624344990763387",
+            "extra": "mean: 1.0860331583610063 msec\nrounds: 903"
+          },
+          {
+            "name": "benchmarks/test_demo.py::test_my_stuff2",
+            "value": 3706.172045458424,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000021358339939734844",
+            "extra": "mean: 269.82017772904226 usec\nrounds: 3646"
+          },
+          {
+            "name": "benchmarks/test_demo.py::test_my_stuff3",
+            "value": 6667.370816988141,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00005081687206199228",
+            "extra": "mean: 149.98415829100847 usec\nrounds: 6109"
           }
         ]
       }
